8865590223cab4e6bf35aa949424494706ea47b8
Rodrigo Vieira <vieiracsa@gmail.com>
2019-01-26 02:23:13 -0200

    Diogo Patch

---
+++ Assets/Editor/RopeScriptEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Rope))]
+public class RopeScriptEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        DrawDefaultInspector();
+
+        Rope myRope = (Rope)target;
+        if (GUILayout.Button("Bake Rope"))
+        {
+            myRope.BakeRope();
+            EditorUtility.SetDirty(myRope);
+        }
+        if (GUILayout.Button("Clear"))
+        {
+            myRope.ClearRope();
+            EditorUtility.SetDirty(myRope);
+        }
+    }
+
+    [MenuItem("GameObject/3D Object/Custom/Rope")]
+    public static void NewRope(){
+        var newRope = 
+            PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath("Assets/RopeBase.prefab", typeof(GameObject)));
+
+        PrefabUtility.DisconnectPrefabInstance(newRope);
+
+        newRope.name = "Rope";
+        Selection.activeGameObject = newRope as GameObject;
+    }
+}
--- Assets/Scripts/FlyingPlayer.cs
+++ Assets/Scripts/FlyingPlayer.cs
@@ -2,10 +2,11 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerInputHandler : MonoBehaviour
+public class FlyingPlayer : MonoBehaviour
 {
 
-    public float playerSpeed = 20f;
+    public float playerSpeed = 10f;
+    public float airSpeed = 20f;
     public float jumpHeight = 3f;
     public float groundDamping = 10f;
     public float airDamping = 5f;
@@ -33,21 +34,18 @@ public class PlayerInputHandler : MonoBehaviour
     {
         _velocity = _controller.velocity;
 
-        Time.timeScale = Input.GetKey(KeyCode.Space) ? 0.1f : 1f;
-
 #if DEBUGMODE
         _renderer.material.color = _controller.isGrounded ? Color.green : Color.red;
 #endif
 
         float horizontalAxis = Input.GetAxis("Horizontal");
-
-        bool jumpKeys = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);// || Input.GetKeyDown(KeyCode.Space);
-        if (jumpKeys && _controller.isGrounded)
-            _velocity.y = Mathf.Sqrt(2f * jumpHeight * -playerGravity.y);
+        float verticalAxis = Input.GetAxis("Vertical");
 
         float movementDamping = _controller.isGrounded ? groundDamping : airDamping;
-
+        if(verticalAxis != 0)
+            _velocity.y = Mathf.Lerp(_velocity.y, verticalAxis * airSpeed, Time.deltaTime * movementDamping);
         _velocity.x = Mathf.Lerp(_velocity.x, horizontalAxis * playerSpeed, Time.deltaTime * movementDamping);
+
         _velocity += playerGravity * Time.deltaTime;
 
         _controller.Move(_velocity * Time.deltaTime);
