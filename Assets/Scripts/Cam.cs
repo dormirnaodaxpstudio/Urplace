56abff22d8062389626defbb16bb762dbfd0f499
Marcelo Chaves <marcelochaves17@gmail.com>
2019-01-25 23:28:42 -0200

    Primeiros scripts e testes de gameplay

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private Character character;
+
+    private void Update()
+    {
+        this.Move();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Floor")) {
+            
+        }
+    }
+
+    private void Move()
+    {
+        this.transform.position = Vector3.Slerp(this.transform.position, new Vector3(character.transform.position.x, character.transform.position.y + 2.2f, this.transform.position.z), speed);
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float speedMovement;
+
+    private void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        this.transform.position += move * speedMovement * Time.deltaTime;
+    }
+}
