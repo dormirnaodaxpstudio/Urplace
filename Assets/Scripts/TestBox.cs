4110373f335385336365e4fe089a4179047c04c7
Marcelo Chaves <marcelochaves17@gmail.com>
2019-01-26 03:46:28 -0200

    Pull trigger

---
+++ Assets/Scripts/TestBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBox : MonoBehaviour
+{
+    [SerializeField] private GameObject frontCollider;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.K))
+            frontCollider.SetActive(true);
+
+        if (Input.GetKeyUp(KeyCode.K))
+            frontCollider.SetActive(false);
+    }
+}
