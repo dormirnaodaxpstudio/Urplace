55258dea1a82cf48d6e4a8ee2803713729fc0382
Marcelo Chaves <marcelochaves17@gmail.com>
2019-01-27 01:01:20 -0200

    Sewers

---
+++ Assets/Scripts/TrapSewer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TrapSewer : MonoBehaviour
+{
+    [SerializeField] private GameObject trap;
+    private PlayerInputHandler player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            trap.GetComponent<Rigidbody>().useGravity = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
