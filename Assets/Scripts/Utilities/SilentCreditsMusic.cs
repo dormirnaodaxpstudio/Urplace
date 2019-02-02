0a6eb8588babb7a853c15877e7954c2d3b81f2c3
Marcelo Chaves <marcelochaves17@gmail.com>
2019-02-02 04:54:49 -0200

    Fix credits

--- Assets/Scripts/Utilities/SilentCreditsMusic.cs
+++ Assets/Scripts/Utilities/SilentCreditsMusic.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SilentCreditsMusic : MonoBehaviour
 {
+    // [SerializeField] private float speedCredits;
+    // [SerializeField] private GameObject credits;
+
     [SerializeField] private AudioSource endPianoMusic1;
     [SerializeField] private AudioSource endPianoMusic2;
+
+    /* Alternative */
+    // private void Update()
+    // {
+    //     credits.transform.position += credits.transform.up * Time.deltaTime * speedCredits;
+    // }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Game");
+    }
 
     public void SilentMusic()
     {
         endPianoMusic1.volume -= 0.25f;
         endPianoMusic2.volume -= 0.25f;
     }
-
 }
